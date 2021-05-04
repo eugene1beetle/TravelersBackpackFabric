e1a9f8d1b84efbaeef73d5c201dfc99952568c27
eugene1beetle <eugene.beetle.01@gmail.com>
2021-05-05 02:38:19 +0300

    rename example mod

---
+++ src/main/java/com/eugene1beetle/travelersbackpackfabric/TravelersBackpackMod.java
@@ -0,0 +1,10 @@
+package com.eugene1beetle.travelersbackpackfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TravelersBackpackMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
